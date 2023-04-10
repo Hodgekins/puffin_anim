3ea7ef1a3f8d5c010303dafa413339726938b6d7
Ben Hodge <bwhodge@gmail.com>
2023-04-10 17:41:58 +0100

    Add container effects

--- js/anim.js
+++ js/anim.js
@@ -1,6 +1,8 @@
 gsap.registerPlugin(ScrollTrigger);
 
-//Fade in (upwards) on scroll
+/**
+ * Elements with .anim-fade-up will fade in and upwards on scroll
+ */
 (function() {
 
   const elements = gsap.utils.toArray(".anim-fade-up");
@@ -23,31 +25,41 @@ gsap.registerPlugin(ScrollTrigger);
   });
 })();
 
-//Highlight on hover
+/**
+ * Elements with .anim-container-fade-up will fade their children in and 
+ * upwards on scroll (staggered)
+ */
 (function() {
-  
-  const elements = document.querySelectorAll('.anim-highlight');
 
-  elements.forEach((el) => {
-    el.addEventListener("mouseover", () => {
-      gsap.to(el, {
-        duration: 1,
-        scale: 1.04,
-      });    
+  const elements = document.querySelectorAll(".anim-container-fade-up");
+
+  elements.forEach((item) => {
+    gsap.set(item.children, {
+      opacity: 0,
+      y: 20
     });
-    el.addEventListener("mouseout", () => {
-      TweenLite.to(el, 1, {
-        scale: 1,
-      });    
-    });
-  }); 
+    gsap.to(item.children, {
+      duration: 1,
+      opacity: 1,
+      y: 0,
+      stagger: 0.1,
+      scrollTrigger: {
+        trigger: item,
+        start: "top 80%",
+        end: "bottom 20%",
+      }
+    })
+  });
 })();
 
-//anim-scroll-slow
+/**
+ * Elements with .anim-scroll-slow will scroll slightly slower creating a 
+ * parallax effect.
+ */
 (function () {
-  items = gsap.utils.toArray(".anim-scroll-slow");
+  elements = gsap.utils.toArray(".anim-scroll-slow");
 
-  items.forEach((item) => {
+  elements.forEach((item) => {
     gsap.set(item, {
       y: -80
     });
@@ -64,11 +76,13 @@ gsap.registerPlugin(ScrollTrigger);
   });
 })();
 
-//anim-scroll-up
+/**
+ * Elements with .anim-scroll-slow will scroll slightly faster creating a parallax effect.
+ */
 (function () {
-  items = gsap.utils.toArray(".anim-scroll-up");
+  elements = gsap.utils.toArray(".anim-scroll-up");
 
-  items.forEach((item) => {
+  elements.forEach((item) => {
     gsap.set(item, {
       y: 20
     });
@@ -85,5 +99,38 @@ gsap.registerPlugin(ScrollTrigger);
   });
 })();
 
+/**
+ * This effect was designed for pretty menus.
+ * .animContainerReplace is the trigger.
+ * [data-anim-hide] is the container of elements that will be hidden.
+ * [data-anim-show] is the container of element that will be shown.
+ */
+(function() {
+  const elements = document.querySelectorAll(".animContainerReplace");
 
+  elements.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      const itemsToHide = document.querySelectorAll(`#${item.dataset.animHide} > *`);
+      const itemsToShow = document.querySelectorAll(`#${item.dataset.animShow} > *`);
+      const tl = gsap.timeline({});
+      tl.to(itemsToHide, {
+        duration: 0.4,
+        stagger: 0.1,
+        opacity: 0,
+        y: -20,
+        onComplete: () => {
+          document.querySelector(`#${item.dataset.animHide}`).classList.add("d-none");
+          document.querySelector(`#${item.dataset.animShow}`).classList.remove("d-none");
+        }
+      }).staggerFromTo(itemsToShow, 0.4, {
+        y: 20,
+        opacity: 0,
+      }, {
+        y: 0,
+        opacity: 1
+      },
+      0.1);
+    });
+  });
+})();
 
